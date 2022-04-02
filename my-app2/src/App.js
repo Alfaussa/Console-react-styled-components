7a01456ae9c177bd165b6712b7f8125fbd44b79c
Alfiya <al.yausheva.com>
2022-04-02 16:47:51 +0200

    5

--- my-app2/src/App.js
+++ my-app2/src/App.js
@@ -1,16 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Fcomponent from './Fcomponent';
 import Ccomponent from './Ccomponent';
-
+import Fcomponent from './Fcomponent';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
+        <Fcomponent />
         <Ccomponent />
-      
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
