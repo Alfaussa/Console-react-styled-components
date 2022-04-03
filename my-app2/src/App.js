209a175eaad5b8dade9de63621a32c01a5c5ce2c
Alfiya <al.yausheva.com>
2022-04-03 22:21:44 +0200

    fin

--- my-app2/src/App.js
+++ my-app2/src/App.js
@@ -4,6 +4,9 @@ import Ccomponent from './Ccomponent';
 import styled from 'styled-components';
 import Title from './Title';
 import Flex from './Flex';
+import Console from './Console';
+import Button from './Button';
+
 const AppWrapper = styled.div`
 width:100%;
 min-height:100vh;
@@ -16,9 +19,12 @@ const App = () => {
   return (
     <AppWrapper> 
 
-    <Flex justify="center">   <Title color={"green"}>Заголовок</Title>
+    <Flex justify="center">   <Title color={"green"}>Test application Console 2022</Title>
     </Flex>
-    
+    <Flex direction="column" margin={"30px 0"}>
+    <Console ></Console>
+    <Button color={"green"}outlined align="flex-end">Send</Button>
+    </Flex>
      
    </AppWrapper> 
   );
--- my-app2/src/index.js
+++ my-app2/src/index.js
@@ -3,22 +3,36 @@ import React from 'react';
  import './index.css';
  import App from './App';
  import reportWebVitals from './reportWebVitals';
- import styled, {createGlobalStyle} from 'styled-components';
+ import styled, {createGlobalStyle, ThemeProvider} from 'styled-components';
 
  const Global = createGlobalStyle`
  * {
    margin:0;
    padding:0;
    box-sizing: border-box;
-   font-family: consolas;
+   font-family: Courier;
  }`
+
+ const theme ={
+   colors: {
+     primary:"green",
+     secondary:"red",
+   },
+   media: {
+     phone: "max-width: 425px",
+     tablet: "(max-width: 768px) and (min-width: 425px)",
+   }
+ }
+
+
+
  const container = document.getElementById('root');
  const root = ReactDOM.createRoot(container);
  root.render(
-     <>
+     <ThemeProvider theme={theme}>
      <Global />
      <App />
-     </>
+     </ThemeProvider>
       );
 
  reportWebVitals();
